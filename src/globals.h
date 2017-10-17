1ea7886d4b7734d7cf2edc417695c11c73c51285
puzcoin <akuma@mud.com.cn>
2017-10-17 22:36:11 +0800

    1

---
+++ src/globals.h
@@ -0,0 +1,25 @@
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+#define GENESIS_TIME_STAMP	"September 26, 2017, Alibaba Culture and Entertainment Group (Ali culture) announced the formal establishment of the game business group, under the open platform business and interactive entertainment division."
+#define DEFAULT_PORT 		22222
+#define DEFAULT_RPC_PORT	33333
+#define GENESIS_BLOCK_TIME	1506425360
+#define GENESIS_BLOCK_NONCE	1526278	
+#define GENESIS_BLOCK_HASH	"0x0000085c71b5fad977230a1877ab0bdd7e6041b2c15246ecd336d8b8219913c1"
+#define GENESIS_BLOCK_MERKLE	"0x5dcfb0324ceaa37d63773b5f6801c2bb80d5313eb2f8be13105422fff9b70466"
+#define PREFIXES_PUBKEY_ADDRESS	(1,56)
+#define PREFIXES_SCRIPT_ADDRESS (1,16)
+#define PREFIXES_SECRET_KEY	(1,190)
+#define PREMINING_STOP_BLOCK	100
+#define PREMINING_REWARD	10000000
+#define MINING_REWARD		5000
+#define MINING_REDUSE		300
+#define MINING_REDUSE_BLOCKS	50000
+#define MASTERNODE_COIN_NEEDED	3000000
+#define COIN_CONF_FILENAME	"puzcoin.conf"
+#define MASTERNODE_CONF_FILENAME	"masternode.conf"
+#define FORCE_MASTERNODE_PAYEE_BLOCKHEIGHT	15000
+#define FORK_15000_NBITS	0x1e0e0ff0
+#define FORK_15000_STEPS	100
+#endif
+
